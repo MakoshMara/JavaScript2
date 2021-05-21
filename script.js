1df1d107cd0c90734fe1facd123228a813c5ac6c
PanovaM <ri5@mail.ru>
2021-05-21 23:03:43 +0700

    DZ3

--- script.js
+++ script.js
@@ -1,27 +1,71 @@
+const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses'
+
+const makeGETRequest = (url) => {
+    let xhr;
+
+    if (window.XMLHttpRequest) {
+        xhr = new XMLHttpRequest();
+    } else if (window.ActiveXObject) {
+        xhr = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    const promise = new Promise((res, rej) => {
+        setTimeout(() => {
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4) {
+                    return xhr.responseText
+                }
+            }
+            res(xhr.onreadystatechange());
+            rej('ERROR')
+        }, 1000)
+    })
+
+    promise // pending
+        .then((data) => {
+                list.goods = JSON.parse(data);
+                list.get_total_cost();
+                list.render()
+            }
+        )
+
+        .catch((err) => {
+            console.log(err)
+        })
+
+    xhr.open('GET', url, true);
+    xhr.send();
+}
 class GoodsItem {
-    constructor(title, price) {
+    constructor(title, price, id_product) {
         this.title = title;
         this.price = price;
+        this.id_product = id_product
     }
 
     render() {
-        return `<div class="goods-item"><img src = "good.jpg"><h3>${this.title}</h3><p>${this.price}</p></div> <br>`;
+        let data = {
+            price: this.price,
+            product_name: this.title,
+            id_product: this.id_product,
+        };
+        data = JSON.stringify(data);
+        return `<div class="goods-item"><img src = "good.jpg"><h3>${this.title}</h3><p>${this.price}</p><br>
+<button class="goodButton" data-product='${data}' id = "${this.id_product}">Купить</button>
+        </div> <br>`;
     }
 }
 
 class GoodsList {
+    _basket = new Basket()
     constructor() {
         this.goods = [];
         this.total_cost = 0;
     }
 
     fetchGoods() {
-        this.goods = [
-            { title: 'Shirt', price: 150 },
-            { title: 'Socks', price: 50 },
-            { title: 'Jacket', price: 350 },
-            { title: 'Shoes', price: 250 },
-        ];
+        makeGETRequest(`${API_URL}/catalogData.json`)
+
     }
 
     get_total_cost(){
@@ -30,32 +74,50 @@ class GoodsList {
 
     render() {
         let listHtml = '';
+        console.log(this.goods)
         this.goods.forEach(good => {
-            const goodItem = new GoodsItem(good.title, good.price);
+            const goodItem = new GoodsItem(good.product_name, good.price, good.id_product);
             listHtml += goodItem.render();
         });
         document.querySelector('.goods-list').innerHTML = listHtml;
         document.querySelector('.goods-total').innerHTML = `Общая стоимость: ${this.total_cost}`
+        this._basket.setAddListeners(this.goods);
     }
-
 
 }
 
 
 class Basket {
-    constructor(user,basket_items) {
+    constructor(user=undefined,basket_items) {
         this.user = user; // корзина у каждого пользователя индивидуальна
         this.basket_items = [];
     }
 
-    add_basket_item(){
-    // тут будут создаваться экземпляры класса Basket_item и добавляться в список basket_items
+    add_basket_item(event){
+        const { target } = event;
+        const { product = {} } = target.dataset;
+        this.basket_items.push(JSON.parse(product));
+        this.render();
     }
 
-    delete_basket_item(){
-    // удаление экземпляра класса Basket_item из списка basket_items
+    delete_basket_item({ target }){
+        const { id } = target.dataset;
+        console.log({id})
+        this.basket_items = this.basket_items.filter((item) => String(item.id_product) !== String(id));
+        console.log(this.basket_items)
+        this.render();
+    }
+    setAddListeners(list = [{ id_product: 123 }, { id_product: 456 }]) {
+        list.forEach((item) => {
+            document.getElementById(`${item.id_product}`).addEventListener('click', (e) => this.add_basket_item(e));
+        })
     }
 
+    setDeleteListeners() {
+        this.basket_items.forEach((item) => {
+            document.getElementById(`${item.id_product}`).addEventListener('click', (e) => this.delete_basket_item(e));
+        })
+    }
     get_quantity_basket_item(){
     // изменение количества конкретного товара в корзине
     }
@@ -68,28 +130,37 @@ class Basket {
     // подсчет общей цены товара в корзине
     }
 
-    render_basket(basket_items){
-    // для каждого товара в корзине будет вызван метод render_basket_item
+    render() {
+        let listHtml = '';
+        this.basket_items.forEach(good => {
+            const goodItem = new BasketItem(good.product_name, good.price, good.id_product);
+            listHtml += goodItem.render();
+        });
+        document.querySelector('.basket').innerHTML = listHtml;
+        this.setDeleteListeners();
     }
-
 }
-
-class Basket_item {
-    constructor(good, quantity = 1, date) {
-        this.good = good;
-        this.quantity = quantity;
-        this.date_add = date; // дата добавления товара, может пригодиться для всяческих скидок
+class BasketItem extends GoodsItem {
+    constructor(...args) {
+        super(...args);
+        this.quantity = 0;
+        this.date_add = 0;
     }
 
     basket_item_cost(quantity, price){
     // подсчет цены каждого конкретного товара в зависимости от количества. Пригодиться для рендера
     }
-    render_basket_item(){
-    // рендер каждого конкретного товара в корзине
+    render() {
+        return `<div class='goods-item'>
+              <h3>${this.title}</h3>
+              <p>${this.price}</p>
+              <button data-id='${this.id_product}' id='${this.id_product}'>Удалить</button>
+            </div>`;
     }
 
 }
+
+
 const list = new GoodsList();
-list.fetchGoods();
-list.get_total_cost()
-list.render();
+list.fetchGoods()
+
